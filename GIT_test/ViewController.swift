841dda1d54914f59973c68a3dda6d32f01c95d7c
Rohit <rohit.g@omgsoftware.net>
2018-06-13 18:44:49 +0530

    Initial Commit

---
+++ GIT_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GIT_test
+//
+//  Created by Praveen Khare on 13/06/18.
+//  Copyright © 2018 Rohit gaikwad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
